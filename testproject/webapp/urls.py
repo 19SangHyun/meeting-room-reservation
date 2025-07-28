d438296dc25f21fea6316c92c05507147776fc7d
이상현 <qhemf0410@naver.com>
2025-07-28 15:49:49 +0900

    signup and login function

---
+++ testproject/webapp/urls.py
@@ -0,0 +1,11 @@
+# webapp/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    # 이후에 회의실 목록 URL 추가 예정
+]
+
--- testproject/webapp/views.py
+++ testproject/webapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# webapp/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
+from .forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'webapp/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('room_list')  # 로그인 후 회의실 목록 페이지로 이동
+        else:
+            return render(request, 'webapp/login.html', {'error': '아이디 또는 비밀번호가 올바르지 않습니다.'})
+
+    return render(request, 'webapp/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
